bf03562d93a88ccbe572fdd94df1769bb3ce559b
sverdoot <lagutin.em@phystech.edu>
2021-03-28 15:23:00 +0300

    fix

--- download_results.sh
+++ download_results.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 gdown https://drive.google.com/uc?id=1fJ55n1LjMMfTmijsBVhnJA6C-tqk0xyt -O out/med/bilin
-gdown https://drive.google.com/uc?id=16AkFTLvH5jeedCeH3vNwC9FxINQvsFfv -O out/med/high
+gdown https://drive.google.com/uc?id=16AkFTLvH5jeedCeH3vNwC9FxINQvsFfv -O out/high/bilin
